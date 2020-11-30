83393025d7888dd81f8863a4aaa4607cf5be6383
Jurph <j.r.parsons@gmail.com>
2020-11-30 12:46:39 -0500

    Sketched in reading the input and executing main()

---
+++ day01/day1p1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# Day 1, Part 1 of Advent of Code 2020
+
+def readstuff():
+    with open('input.txt', "r") as file:
+        line = file.readline
+    return
+
+def dostuff():
+    return
+
+def main():
+    dostuff()
+
+if __name__ == "__main__":
+    main()
--- day01/day1part1.py
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/python3
-# Day 1, Part 1 of Advent of Code 2020
