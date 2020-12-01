477d52f05a0ce4b8e7d38522846cafdad88403be
Jurph <j.r.parsons@gmail.com>
2020-12-01 16:56:51 -0500

    500x total improvement with list trimming first

--- day01/day1p2.py
+++ day01/day1p2.py
@@ -19,9 +19,11 @@ class Problem():
 def main():
     goal = 2020
     p = Problem('input.txt')
-    unsorted = p.integers 
     frontways = sorted(p.integers, reverse=False)
     backways = sorted(p.integers, reverse=True)
+    while (2 * frontways[0]) + backways[0] > goal:
+        backways.remove(backways[0])
+        frontways.remove(frontways[-1])
     for firstnumber in frontways:
         for secondnumber in frontways:
             for thirdnumber in backways: 
@@ -32,8 +34,11 @@ def main():
     return
 
 if __name__ == "__main__":
-    tic = time.perf_counter()
-    main()
-    toc = time.perf_counter()
-    elapsed = toc - tic 
-    print("Main() executed in {:.6f} seconds".format(elapsed))
+    total = 0
+    for _ in range(1000):
+        tic = time.perf_counter()
+        main()
+        toc = time.perf_counter()
+        elapsed = toc - tic 
+        total += elapsed
+    print("Main() executed in an average of {:.6f} seconds".format(total/1000))
