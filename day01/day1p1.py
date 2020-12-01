02ecb2f2f2b923f06322a4f40975dfacd61f3ddc
Jurph <j.r.parsons@gmail.com>
2020-12-01 08:56:08 -0500

    Successfully solved day 1 part 1

--- day01/day1p1.py
+++ day01/day1p1.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
 # Day 1, Part 1 of Advent of Code 2020
 
-def readstuff():
-    with open('input.txt', "r") as file:
-        line = file.readline
-    return
-
-def dostuff():
-    return
+# Sketch in a generic class for holding the input
+class Problem():
+    def __init__(self, filename):
+        strings = []
+        integers = []
+        with open(filename, "r") as file:
+            for line in file:
+                strings.append(line)
+                integers.append(int(line))
+        self.strings = strings
+        self.integers = integers
+        return
 
 def main():
-    dostuff()
+    goal = 2020
+    p = Problem('input.txt')
+    for firstnumber in p.integers:
+        for secondnumber in p.integers:
+            if firstnumber + secondnumber == goal:
+                print("Found {} and {} which sum to {}.".format(firstnumber, secondnumber, goal))
+                print("Product is {}.".format(firstnumber * secondnumber))
+                return
+    return
 
 if __name__ == "__main__":
     main()
