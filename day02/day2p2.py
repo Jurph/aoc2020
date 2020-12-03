a2bd3cb134a6afda483fb99044a7959721b853a4
Jurph <j.r.parsons@gmail.com>
2020-12-02 20:43:08 -0500

    cleanup pass

--- day02/day2p2.py
+++ day02/day2p2.py
@@ -24,16 +24,15 @@ class Entry():
         return
 
     def isvalid(self):
-        # Exactly one of the cases must be met, so we return false if both are! 
-
+        # Exactly one of the cases must be met, so we return false if both are
         if self.password[self.firstposition-1] == self.target_character and self.password[self.secondposition-1] == self.target_character:
-            print("{} has a \"{}\" at position {} and {}    : INVALID".format(self.password, self.target_character, self.firstposition, self.secondposition))
+            # print("{} has a \"{}\" at position {} and {}    : INVALID".format(self.password, self.target_character, self.firstposition, self.secondposition))
             return False
         elif self.password[self.firstposition-1] == self.target_character and self.password[self.secondposition-1] != self.target_character:
-            print("{} has a \"{}\" at position {} but not {}: VALID".format(self.password, self.target_character, self.firstposition, self.secondposition))
+            # print("{} has a \"{}\" at position {} but not {}: VALID".format(self.password, self.target_character, self.firstposition, self.secondposition))
             return True
         elif self.password[self.firstposition-1] != self.target_character and self.password[self.secondposition-1] == self.target_character:
-            print("{} has a \"{}\" at position {} but not {}: VALID".format(self.password, self.target_character, self.secondposition, self.firstposition))
+            # print("{} has a \"{}\" at position {} but not {}: VALID".format(self.password, self.target_character, self.secondposition, self.firstposition))
             return True
         else:
             return False
